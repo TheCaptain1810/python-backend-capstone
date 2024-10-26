19995fd46152b33f82a343f4b2c9308ae0f97ae1
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2024-10-26 17:58:47 +0530

    done

---
+++ config.py
@@ -0,0 +1,4 @@
+# config.py
+groq_apikey = "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"
+mongodb_uri = "mongodb+srv://thecaptain:thecaptain@cluster0.nhf1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
