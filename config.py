ac0c4f5b612988faaab527bbd8aa1e2056586d82
Pranav Pise <160746099+TheCaptain1810@users.noreply.github.com>
2025-04-14 20:26:53 +0530

    Delete config.py
    Please don't copy my keys, I beg you, I was a fool. :')

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-groq_apikey = "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"
-mongodb_uri = "mongodb+srv://thecaptain:thecaptain@cluster0.nhf1y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
